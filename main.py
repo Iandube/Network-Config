be8e916bec075e3b1a8c92310be3d966a180d012
Iandube <iandube3822@gmail.com>
2026-03-17 21:17:13 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
-
-
 import netmiko
 import jinja2
+from datetime import datetime
+
+current_time = datetime.now().strftime("%H:%M:%S %d %b %Y")
 
 config = {
     'switch': {
         'hostname': '',
+        'clock set': current_time,
+        'banner motd': '',
         'credentials': {
             'priv_exec_pw': '',
             'console_pw': '',
             'vty_pw': ''
             },
-        'vlans': [
-            {'vlan_id': None, 'vlan_name': None}
-            ],
-        'interfaces': [
-            {'mode': 'access', 'start': None, 'stop': None},
-            {'mode': 'trunk', 'start': None, 'stop': None},
-            {'mode': 'etherchannel', 'start': None, 'stop': None}
-        ]
+        'vlans': [],
+        'interfaces range': [],
+        'interfaces singular': [],
     }
 }
+print(config)
 
 def switch_config(config):
     
@@ -46,7 +45,7 @@ def switch_config(config):
             break
         else:
             print("Please enter either Y or N.")
-            break
+            continue
     
     # Asks for a console password
     while True:
@@ -60,7 +59,7 @@ def switch_config(config):
             break
         else:
             print("Please enter either Y or N.")
-            break
+            continue
     
     # Asks for a VTY line password
     while True:
@@ -74,13 +73,66 @@ def switch_config(config):
             break
         else:
             print("Please enter either Y or N.")
+            continue
+    
+    # Asks for the vlan count
+    while True:
+        try:
+            number_of_vlans = int(input("Please enter a NUMBER of vlans you would like to create: ")).strip()
+            if number_of_vlans < 1:
+                print("Please enter a number above 0.")
+                continue
             break
-            
+        except ValueError:
+            print("Please enter an integer.")
+   
+    # Collects the vlans
+    vlans = []
+    for i in range(number_of_vlans):
+        while True:
+            try:
+                vlan_id = int(input(f"Please enter the VLAN ID For VLAN # {i+1}: ").strip())
+                break
+            except ValueError:
+                print("Please enter a proper vlan ID")
         
+        vlan_name = input(f"Please enter a name for VLAN ID{vlan_id}: ").strip()
+        vlans.append({
+            'vlan_id': vlan_id,
+            'vlan_name': vlan_name
+        })
+        config['switch']['vlans'] = vlans
 
 
-
-
+    # Interfaces mode start
+    while True:
+        interf_acc_quest = input("Would you like to configure access ports for this switch?\nY/N: ")
+        if interf_acc_quest == 'N':
+            print("Skipping interface access ports...").strip()
+            break
+        elif interf_acc_quest == 'Y':
+            port_range_conf = input("Are you configuring a range of ports? or single port? Type: \nrange/single: ")
+            if port_range_conf == 'range':
+                port_start = input("What is the START range of ports you would like to configure? (Format: G1/0/1)")
+                port_end = input("What is the END port you would like to configure? (Format: Single integer: 1)")
+                config['switch']['interfaces range'].append({
+                    'mode': 'access',
+                    'port_start': port_start,
+                    'port_end': port_end
+                })
+            elif port_range_conf == 'single':
+                port_num = input("Please input the singular port number in this format: G1/0/1\n")
+                config['switch']['interface singular'].append({
+                    'mode': 'access',
+                    'port_num': port_num
+                })
+        else: 
+            try:
+                print("Please enter a valid option")
+            except: ValueError
+            break
+    
+    
 
 
 def main_menu():
